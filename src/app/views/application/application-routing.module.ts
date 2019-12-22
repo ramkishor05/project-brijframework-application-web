a3161af6c6fd9327380baf7e34eedcee46078abc
phpoms <phpoms>
2019-12-22 20:30:54 +0530

    project-brijframework-application-web

---
+++ src/app/views/application/application-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ApplicationComponent } from './application.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ApplicationComponent,
+    data: {
+      title: 'Application'
+    }
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ApplicationRoutingModule {}
---
+++ src/app/views/application/application.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+
+import { ApplicationComponent } from './application.component';
+import { ApplicationRoutingModule } from './application-routing.module';
+
+@NgModule({
+  imports: [
+    ApplicationRoutingModule
+  ],
+  declarations: [ ApplicationComponent ]
+})
+export class ApplicationModule { 
+
+}
